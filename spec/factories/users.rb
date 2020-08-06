6e0fc22d310cea3115095de5b37f3021c1a71ff2
oselimi <osman.94@live.com>
2020-08-06 06:15:09 +0200

    User model, presence and email uniqueness

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    first_name { "Osman" }
+    last_name  { "Selimi" }
+    email {"osman@live.com"}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    describe "Should be present" do
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
+      it { should validate_presence_of(:email) }
+    end
+
+    describe "Email should be uniqueness" do
+      it { should validate_uniqueness_of(:email) }
+    end
+end
